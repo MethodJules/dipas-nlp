c5beca6b8a45450dd4f8ee09f99950ad62ddd790
MethodJules <julien.hofer@gv.hamburg.de>
2023-07-13 08:34:52 +0200

    HOT-FIX: Added file extension to synomyMethod file

---
+++ synonymMethod.py
@@ -0,0 +1,21 @@
+import requests
+import json
+
+def get_synonyms(word):
+    API_URL = "https://www.openthesaurus.de/synonyme/search?q={}&format=application/json"
+    response = requests.get(API_URL.format(word))
+
+    if response.status_code == 200:
+        # Konvertierung zu JSON-Datei
+        data = json.loads(response.text)
+        
+        print("Statuscode: 200")
+        
+        # Synonyme für das Wort werden extrahiert
+        synonyms = [term['term'] for term in data['synsets'][0]['terms']]
+        return synonyms
+
+    else:
+        return "Fehler: Die Anfrage war nicht erfolgreich. Statuscode: " + str(response.status_code)
+
+print(get_synonyms('Informatik'))
